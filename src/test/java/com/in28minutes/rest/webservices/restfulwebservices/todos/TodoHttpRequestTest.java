da05e1387ca3905fa6d8205c5d044c05d381e13e
Jinho Yim <jinhoyim@outlook.com>
2024-07-15 23:05:49 +0900

    Todo API 항목 생성 및 수정 추가

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/todos/TodoHttpRequestTest.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/todos/TodoHttpRequestTest.java
@@ -40,4 +40,41 @@ public class TodoHttpRequestTest {
         ResponseEntity<Todo> response = this.restTemplate.getForEntity(url, Todo.class);
         assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
     }
+
+    @Test
+    void updateTodo() {
+        String username = "in28minutes";
+        String url = "http://localhost:" + port + "/users/" + username + "/todos/3";
+
+        Todo origin = this.restTemplate.getForObject(url, Todo.class);
+        Todo requestTodo = new Todo(3,
+                username,
+                origin.getDescription() + " 33",
+                origin.getTargetDate(),
+                origin.isDone());
+
+        this.restTemplate.put(url, requestTodo);
+
+        Todo actual = this.restTemplate.getForObject(url, Todo.class);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(requestTodo);
+    }
+
+    @Test
+    void addTodo() {
+        String username = "in28minutes";
+        String url = "http://localhost:" + port + "/users/" + username + "/todos";
+        Todo todo = new Todo();
+        todo.setDescription("Learn Full Stack Development");
+        todo.setTargetDate(LocalDate.of(2025, 2, 5));
+        todo.setDone(false);
+
+        Todo actual = this.restTemplate.postForObject(url, todo, Todo.class);
+
+        Todo expected = new Todo(4,
+                username,
+                "Learn Full Stack Development",
+                LocalDate.of(2025, 2, 5),
+                false);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
 }
