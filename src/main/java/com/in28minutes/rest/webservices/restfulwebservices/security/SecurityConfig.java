97290e8a81a6994be18e131f4d3e89dfc9810888
Jinho Yim <jinhoyim@outlook.com>
2024-07-17 09:22:01 +0900

    기본 인증(Basic Auth)에서 JWT 토큰 인증으로 변경

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SecurityConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
