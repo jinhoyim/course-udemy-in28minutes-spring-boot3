70fca7429b55ef8173ae8ea89134462eed850ea3
Jinho Yim <jinhoyim@outlook.com>
2024-07-26 15:33:15 +0900

    JPA와 H2를 이용한 Todo 모델 영속화

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
@@ -1,8 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String description;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into TODO (id, is_done, target_date, description, username)
+values (1, false, current_date(), 'Get AWS Certified', 'in28minutes');
+
+insert into TODO (id, is_done, target_date, description, username)
+values (2, false, current_date(), 'Learn DevOps', 'in28minutes');
+
+insert into TODO (id, is_done, target_date, description, username)
+values (3, false, current_date(), 'Learn Full Stack Development', 'in28minutes');
